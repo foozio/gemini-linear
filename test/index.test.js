2067cb901689bd720c9962627dd4bd3c6bed68ec
Ubuntu <ubuntu@localhost.localdomain>
2025-12-30 16:29:22 +0800

    test(index): Add test for missing LINEAR_API_KEY

--- test/index.test.js
+++ test/index.test.js
@@ -7,4 +7,29 @@ describe('index.js', () => {
   it('should be testable', () => {
       assert.ok(true);
   });
+
+  it('should fail if LINEAR_API_KEY is missing', (t, done) => {
+    const env = { ...process.env };
+    delete env.LINEAR_API_KEY;
+
+    const child = child_process.spawn('node', [path.resolve(__dirname, '../index.js')], {
+      env: env,
+      stdio: 'pipe'
+    });
+
+    let stderr = '';
+    child.stderr.on('data', (data) => {
+      stderr += data.toString();
+    });
+
+    child.on('close', (code) => {
+      try {
+        assert.strictEqual(code, 1, 'Process should exit with code 1');
+        assert.match(stderr, /LINEAR_API_KEY environment variable is not set/, 'Stderr should contain error message');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
 });
