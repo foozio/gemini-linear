7a0617f8bdd291d69abd524e9ce3dfd5434d9315
Ubuntu <ubuntu@localhost.localdomain>
2025-12-30 16:28:39 +0800

    chore(test): Create initial test suite for index.js

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+const { test, describe, it, before, after, mock } = require('node:test');
+const assert = require('node:assert');
+const child_process = require('node:child_process');
+const path = require('node:path');
+
+describe('index.js', () => {
+  it('should be testable', () => {
+      assert.ok(true);
+  });
+});
